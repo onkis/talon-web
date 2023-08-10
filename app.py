fa7ec431583cf90235fdaeedf02dfb182fcd2158
Mike Ball <mike.ball3@gmail.com>
2023-08-10 11:09:23 -0400

    extract text without signature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from talon import quotations
+from talon.signature.bruteforce import extract_signature
 
 from flask import Flask, Response, request
 
@@ -10,13 +11,18 @@ app = Flask(__name__)
 @app.route('/reply/extract_from_html', methods=['POST'])
 def extract_from_html():
     html = request.get_data(as_text=True)
-    return quotations.extract_from_html(html)
+    msg = quotations.extract_from_html(html)
+    text, signature = extract_signature(msg)
+    return text
 
 @app.route('/reply/extract_from_plain', methods=['POST'])
 def extract_from_plain():
-    html = request.get_data(as_text=True)
-    return quotations.extract_from_plain(html)
+    data = request.get_data(as_text=True)
+    msg = quotations.extract_from_plain(data)
 
+    text, signature = extract_signature(msg)
+
+    return text
 
 @app.route('/health')
 def health():

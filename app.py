b4adb2d4b4158803ec073cfdad656adc576b391e
Mike Ball <mike.ball3@gmail.com>
2023-08-10 12:32:46 -0400

    json

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ app = Flask(__name__)
 
 @app.route('/reply/extract_from_html', methods=['POST'])
 def extract_from_html():
-    html = request.get_data(as_text=True)
-    msg = quotations.extract_from_html(html)
+    body = request.get_json()
+    msg = quotations.extract_from_html(body.msg)
     text, signature = extract_signature(msg)
     return text
 
 @app.route('/reply/extract_from_plain', methods=['POST'])
 def extract_from_plain():
-    data = request.get_data(as_text=True)
-    msg = quotations.extract_from_plain(data)
+    body = request.get_json()
+
+    msg = quotations.extract_from_plain(body.msg)
 
     text, signature = extract_signature(msg)
 
